19991d4dad2a4d41644f97e272a4ced94c4bc852
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-02-08 00:24:26 +0100

    Finished "Praxis 2" of the project.

--- src/js/main.js
+++ src/js/main.js
@@ -3,18 +3,29 @@
 
 // Eingabedaten holen
 let titel_1 = prompt("Titel: ");
-console.log(titel_1)
+console.log(`Titel: ${titel_1}`)
+let typ_1 = prompt("Typ (Einnahme / Ausgabe): ");
+console.log(`Typ: ${typ_1}`)
+let betrag_1 = parseInt(prompt("Betrag (in Cent): "));
+console.log(`Betrag: ${betrag_1} ct`)
+let datum_1 = prompt("Datum (jjjj-mm-tt): ");
+console.log(`Datum: ${datum_1}`)
 
-let typ = prompt("Typ (Einnahme / Ausgabe): ");
-console.log(typ)
+let titel_2 = prompt("Titel: ")
+console.log`Titel: ${titel_2}`
+let typ_2 = prompt("Typ (Einnahme / Ausgabe): ")
+console.log(`Typ: ${typ_2}`)
+let betrag_2 = parseInt(prompt("Betrag (in Cent): "))
+console.log(`Betrag: ${betrag_2} ct`)
+let datum_2 = prompt("Datum (jjjj-mm-tt)")
+console.log(`Datum: ${datum_2}`)
 
-let betrag = prompt("Betrag (in Cent): ");
-console.log(betrag)
-
-let datum = prompt("Datum (jjjj-mm-tt): ");
-console.log(datum)
 
 //  Gesamtbilanz
 let einnahmen;
 let ausgaben;
-let bilanz;
+let bilanz = betrag_1 + betrag_2
+console.log(`Bilanz: ${ bilanz} ct`)
+
+let positiv = bilanz >= 0;
+console.log(`Bilanz ist positiv: ${positiv}`)
