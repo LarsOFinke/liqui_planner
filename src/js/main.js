29cdc5ccde1d53b23b7db3e1964bd362c0994345
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-02-08 00:10:54 +0100

    Finished "Praxis 1" from the project of the Udemy course for JavaScript.

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+"use strict";
+
+
+// Eingabedaten holen
+let titel_1 = prompt("Titel: ");
+console.log(titel_1)
+
+let typ = prompt("Typ (Einnahme / Ausgabe): ");
+console.log(typ)
+
+let betrag = prompt("Betrag (in Cent): ");
+console.log(betrag)
+
+let datum = prompt("Datum (jjjj-mm-tt): ");
+console.log(datum)
+
+//  Gesamtbilanz
+let einnahmen;
+let ausgaben;
+let bilanz;
